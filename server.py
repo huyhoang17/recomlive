ce6a25975b56c174478d15e2c9c486826a9e2f6d
Gregory Arefyev <gregory@recom.live>
2019-11-11 20:56:05 +0000

    WIP

--- server.py
+++ server.py
@@ -4,13 +4,13 @@ import os, sys
 
 Server(
         os.getenv('RECOM_SERVER_HOST',                  'localhost'),
-        int(os.getenv('RECOM_SERVER_PORT',              5000)),
+        int(os.getenv('RECOM_SERVER_PORT',              5006)),
         os.getenv('RECOM_SERVER_LOGFILE',               'var/log/server.log'),
         os.getenv('RECOM_SERVER_PIDFILE',               'var/run/server.pid'),
         int(os.getenv('RECOM_SERVER_QLIMIT',            10000)),
-        int(os.getenv('RECOM_SERVER_DOCLIMIT',          2000)),
-        int(os.getenv('RECOM_SERVER_PERSONLIMIT',       2000)),
-        int(os.getenv('RECOM_SERVER_RECSLIMIT',         5)),
-        int(os.getenv('RECOM_SERVER_INVALIDATEAFTER',   5)),
+        int(os.getenv('RECOM_SERVER_DOCLIMIT',          100)),
+        int(os.getenv('RECOM_SERVER_PERSONLIMIT',       100)),
+        int(os.getenv('RECOM_SERVER_RECSLIMIT',         1)),
+        int(os.getenv('RECOM_SERVER_INVALIDATEAFTER',   0)),
         ).command(sys.argv[1])
 
